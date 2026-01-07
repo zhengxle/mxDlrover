f5e8f4d76012c477d2afdcd36c146c2b42aa1a89
xlzheng <xiaolerzheng@gmail.com>
2026-01-07 16:58:39 +0800

    remove no use import pkg

--- go/elasticjob/api/v1alpha1/groupversion_info.go
+++ go/elasticjob/api/v1alpha1/groupversion_info.go
@@ -20,8 +20,6 @@ limitations under the License.
 package v1alpha1
 
 import (
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"sigs.k8s.io/controller-runtime/pkg/scheme"
 )
