7649faa38841e1cf876c4a9ee912977ff701dba4
xlzheng <xiaolerzheng@gmail.com>
2026-01-07 16:36:59 +0800

    remove duplication var in api pkg

--- go/elasticjob/api/v1alpha1/register.go
+++ go/elasticjob/api/v1alpha1/register.go
@@ -6,19 +6,10 @@ import (
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-// SchemeGroupVersion is group version used to register these objects.
-var SchemeGroupVersion = schema.GroupVersion{Group: "elastic.iml.github.io", Version: "v1alpha1"}
-
 // Resource takes an unqualified resource and returns a Group qualified GroupResource
 func Resource(resource string) schema.GroupResource {
 	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
-
-var (
-	// SchemeBuilder 保持与 Kubebuilder 兼容
-	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
-	AddToScheme        = SchemeBuilder.AddToScheme
-)
 
 // addKnownTypes 是 client-gen 识别的核心
 func addKnownTypes(scheme *runtime.Scheme) error {
